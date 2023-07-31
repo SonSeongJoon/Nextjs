ad5b99d6545a291940dbd35d653fdc9fd93e9250
손성준 <sonsj97@gmail.com>
2023-07-31 12:45:46 +0900

    layout-products

--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -1,8 +1,20 @@
+import {notFound} from 'next/navigation';
 type Props = {
     params: {
         slug: string;
     }
 }
 export default function PantsPage({params}: Props) {
+    if (params.slug === "nothing") {
+        notFound();
+    }
     return <h1>{params.slug} 제품 설명 페이지</h1>;
+}
+
+// 정적으로 만들고 싶은게 있을 때
+export function generateStaticParams() {
+    const product = ['pants', 'skirt'];
+    return product.map(product => ({
+        slug: product,
+    }));
 }

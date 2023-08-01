2059c29f811cc7e5e76fb50eeb8bb7b94681afeb
손성준 <sonsj97@gmail.com>
2023-08-01 10:14:41 +0900

    api

--- note/src/app/layout.tsx
+++ note/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         <header className={styles.header}>
             <h1>Demo Note</h1>
             <nav className={styles.nav}>
-                <Link href='/products'>Products</Link>
+                <Link href='/api/products/route'>Products</Link>
                 <Link href='/contact'>Contact</Link>
                 <Link href='/about'>About</Link>
             </nav>

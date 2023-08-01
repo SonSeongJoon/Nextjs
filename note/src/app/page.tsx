4676fec25adc4a5fd7b3b533cfa4ba55901b46e1
손성준 <sonsj97@gmail.com>
2023-08-01 11:16:06 +0900

    Image

--- note/src/app/page.tsx
+++ note/src/app/page.tsx
@@ -5,10 +5,12 @@ import type {metadata} from "@/app/layout";
 import os from 'os'
 import Counter from "@/app/component/Counter"; //노드 api
 export default function Home() {
-    console.log("안녕 - 서버에서만 동작");
-    console.log(os.hostname());
     return (
         <>
+            <Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
+            alt="shop"
+            width={400}
+            height={400}/>
             <h1>홈페이지다.</h1>
             <Counter />
         </>
--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -5,13 +5,14 @@ import {Simulate} from "react-dom/test-utils";
 import change = Simulate.change;
 import MeowArticle from "@/app/component/MeowArticle";
 import error = Simulate.error;
-
+import clothesImage from '../../../public/images/clothes.jpg'
+import Image from 'next/image'
 export default async function ProductsPage() {
-    throw new Error();
     const products = await getProducts();
     return (
         <>
             <h1>제품 소개 페이지!</h1>
+            <Image src={clothesImage} alt="Clothes" width={200} height={200} priority/>
             <ul>
                 {products.map((product, index) => (
                     <li key={index}>

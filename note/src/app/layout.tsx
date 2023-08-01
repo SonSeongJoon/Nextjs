95cef7bfd7caa2cccb29563ddeb17a4d26c52131
손성준 <sonsj97@gmail.com>
2023-08-01 11:44:45 +0900

    font

--- note/src/app/layout.tsx
+++ note/src/app/layout.tsx
@@ -3,9 +3,14 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import styles from './layout.module.css'
 import Link from 'next/link';
-
 const inter = Inter({subsets: ['latin']})
-
+import {Open_Sans} from 'next/font/google';
+import {Nanum_Gothic} from 'next/font/google'
+const sans = Open_Sans({subsets: ['latin']});
+const gothic = Nanum_Gothic({
+    weight: '700',
+    subsets: ['latin']
+})
 export const metadata: Metadata = {
     title: '멋진 제품 사이트',
     description: '멋진 제품을 판매하는 곳입니다.',
@@ -20,10 +25,10 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html lang="en" className={sans.className}>
         <body className={inter.className}>
         <header className={styles.header}>
-            <h1>Demo Note</h1>
+            <h1 className={gothic.className}>Demo Note</h1>
             <nav className={styles.nav}>
                 <Link href='/products'>Products</Link>
                 <Link href='/contact'>Contact</Link>
